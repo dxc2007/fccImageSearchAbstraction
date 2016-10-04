9018c4393ac03f4db5e7201c5ddb8f22b4cb81d3
Xiaochen Du <duxiaochen1996@outlook.com>
2016-10-04 09:02:14 +0000

    secondary findings available

--- server.js
+++ server.js
@@ -14,16 +14,25 @@ var port = process.env.PORT || 8080;
 
 // app.use(express.static(path.join(__dirname, '/public')));
 
+app.set('json spaces', 2);
 
 app.get('/', function(req, res, err) {
     request('https://www.googleapis.com/customsearch/v1?cx=007771692252375537010:q4jjymtgvqg&key=AIzaSyAfLBv6rrMMjhiKtteq_Dw4AjGbVveB0Vw&searchType=image&q=great%20barrier%20reef&start=16', 
     function(error, response, body) {
         if (!error && response.statusCode == 200) {
-            res.send(JSON.parse(body));
-        }
+            var content = JSON.parse(body)['items'];
+            }
+            var info = [];
+            content.forEach(function(data) {
+                info.push("Title: " + data['title'] + " Snippet: " + data['snippet']);
+            });
+            res.send(info);
     });
 });
+// startIndex = 
+// req.params
+// items[0][title,link,snippet]
 
 app.listen(port, function() {
-    console.log('You are listening on: ${port}');
-})
+    console.log(`You are listening on: ${port}`);
+});
